f812586d611f5f87d2c579d0a6bbc314856a39e9
Carl J <carlj1881@yahoo.com>
2020-09-26 00:03:56 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+require"pry"
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @genre = genre
+    @artist = artist
+    
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_counter = {}
+      #binding.pry
+    # @@genres.each do |genre, count|
+    genre_counter.merge!(@genre: @@count)
+    genre_counter
+  end
+end
+end
