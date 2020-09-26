a5d3e872a88ff343a2bffb8714df442b6b4d60f4
Carl J <carlj1881@yahoo.com>
2020-09-26 00:17:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,9 +38,6 @@ class Song
     @@genres.each do |genre|
       if genre_counter = {}
         genre_counter[genre] = @@count
-      else 
-        genre_counter[genre] << @@count
-      end
   end
     genre_counter
   end
